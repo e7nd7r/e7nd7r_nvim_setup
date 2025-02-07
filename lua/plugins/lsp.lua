1fb6b1a8584dc560947cd20342aec638216a79db
M. Esteban Perez Dorador <esteban5003@gmail.com>
2025-02-06 22:13:55 -0800

    Add hits
    Inline hints sometimes are hidden on the right of the window. The commit
    introduces a shortcut to show floating hit when ctrl + h is pressed.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -108,6 +108,9 @@ return {
                     --  For example, in C this would take you to the header.
                     map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+                    -- Show current error or warning hint in a floating popup.
+                    map('<leader>h', vim.diagnostic.open_float,'[H]ints')
+
                     -- The following two autocommands are used to highlight references of the
                     -- word under your cursor when your cursor rests there for a little while.
                     --    See `:help CursorHold` for information about when this is executed
