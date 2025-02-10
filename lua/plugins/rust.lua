5a0020783a3c384d6d42199c1f7719d4ecc3213e
M. Esteban Perez Dorador <esteban5003@gmail.com>
2025-02-09 19:49:16 -0800

    remove useless config

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,20 +1,3 @@
-vim.g.rustaceanvim = function()
-    local mason_registry = require("mason-registry")
-
-    -- Update this path
-    local codelldb = mason_registry.get_package("codelldb")
-    local extension_path = codelldb:get_install_path() .. "/extension/"
-    local codelldb_path = extension_path .. "adapter/codelldb"
-    local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-
-    local cfg = require('rustaceanvim.config')
-    return {
-        dap = {
-            adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-    }
-end
-
 return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
@@ -36,6 +19,6 @@ return {
             },
 
         }
-   end
+    end
 }
 
