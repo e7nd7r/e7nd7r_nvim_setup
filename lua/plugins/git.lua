299bee9a90aa46934df26c362f1798e258b566fd
M. Esteban Perez Dorador <esteban5003@gmail.com>
2025-03-02 16:10:53 -0700

    Git: Remove duplicated config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -104,28 +104,5 @@ return {
         config = function()
         end
     },
-    {
-        -- Adds git related signs to the gutter, as well as utilities for managing changes
-        'lewis6991/gitsigns.nvim',
-        config = function ()
-            require('gitsigns').setup()
-
-            vim.keymap.set('n', '<leader>hp', ':Gitsigns preview_hunk<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '[c', ':Gitsigns prev_hunk<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', ']c', ':Gitsigns next_hunk<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>hs', ':Gitsigns stage_hunk<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk<CR>', { noremap = true, silent = true })
-            vim.keymap.set('n', '<leader>hr', ':Gitsigns reset_hunk<CR>', { noremap = true, silent = true })
-        end,
-        opts = {
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-        },
-    },
 }
 
