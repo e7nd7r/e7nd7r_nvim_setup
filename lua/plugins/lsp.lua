ca7409bc80f5bc0bdf59c5a343c837e599c45482
M. Esteban Perez Dorador <esteban5003@gmail.com>
2025-03-02 16:12:05 -0700

    Solve warning missing fields warning
    using setup_handlers instead of setup and handlers property.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -235,6 +235,7 @@
                         },
                     },
                 },
+                terraformls = {},
             }
 
             -- Ensure the servers and tools above are installed
@@ -251,19 +252,18 @@
             vim.list_extend(ensure_installed, {
                 'stylua', -- Used to format Lua code
             })
+
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-            require('mason-lspconfig').setup {
-                handlers = {
-                    function(server_name)
-                        local server = servers[server_name] or {}
-                        -- This handles overriding only values explicitly passed
-                        -- by the server configuration above. Useful when disabling
-                        -- certain features of an LSP (for example, turning off formatting for ts_ls)
-                        server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-                        require('lspconfig')[server_name].setup(server)
-                    end,
-                },
+            require('mason-lspconfig').setup_handlers {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    -- This handles overriding only values explicitly passed
+                    -- by the server configuration above. Useful when disabling
+                    -- certain features of an LSP (for example, turning off formatting for ts_ls)
+                    server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+                    require('lspconfig')[server_name].setup(server)
+                end,
             }
         end,
     }
