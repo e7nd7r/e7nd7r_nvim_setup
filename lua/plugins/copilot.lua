d127f6e5a0556f64b5be28dc381d42a1c1af77b5
M. Esteban Perez Dorador <esteban5003@gmail.com>
2025-03-16 00:35:41 -0600

    Improve copilot keybindings

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,20 @@
 return {
-    "github/copilot.vim",
-    config = function()
-        vim.keymap.set('i', '<C-A>', 'copilot#Accept("\\<CR>")', {
-          expr = true,
-          replace_keycodes = false
-        })
+	"github/copilot.vim",
+	config = function()
+		vim.keymap.set("i", "<C-S>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+			desc = "Copilot accept suggestion",
+		})
 
-        vim.g.copilot_no_tab_map = true
-    end
+		vim.keymap.set("i", "<C-W>", "<Plug>(copilot-accept-word)", {
+			desc = "Copilot accept word",
+		})
+
+		vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-line)", {
+			desc = "Copilot accept line",
+		})
+
+		vim.g.copilot_no_tab_map = true
+	end,
 }
-
